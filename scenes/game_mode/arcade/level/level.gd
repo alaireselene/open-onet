238add028fc69a76dddb886638bcfc40a1257200
Truong-Son Nguyen <son.nt227148@sis.hust.edu.vn>
2024-12-27 23:03:04 +0700

    Remove Git plugin to use VSCode instead

--- scenes/game_mode/arcade/level/level.gd
+++ scenes/game_mode/arcade/level/level.gd
@@ -1,10 +1,16 @@
 extends Node2D
 @onready var pause_menu = $PauseMenu
 @onready var countdown_label = $"Background/Left panel/Time left/Countdown"
+@onready var score_label = $"Background/Left panel/Ultities/Score/Score number"
+@onready var health_label = $"Background/Left panel/Ultities/Score/Health/Health number"
 @onready var finish_scene = $Finish
 
 var paused = false
 var time_left = 10 # 5 phút tính bằng giây
+var current_score = 0
+var current_health = 5
+var last_update = 0
+
 func _ready():
 	print("Scene reloaded")
 	$ScoreTimer.start() # Bắt đầu bộ đếm thời gian
@@ -14,6 +20,7 @@ func _process(delta: float):
 	if Input.is_action_just_pressed("ui_pause"):
 		pauseMenu()
 		
+# Display pause menu
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
@@ -26,6 +33,7 @@ func pauseMenu():
 func _on_pause_button_pressed():
 	pauseMenu()
 
+# Display finish popup
 func _on_score_timer_timeout():
 	if not paused and time_left > 0:
 		time_left -= 1
@@ -40,3 +48,12 @@ func update_countdown_label():
 
 func go_to_finish_scene():
 	finish_scene.show()
+	
+# Update health label
+func update_health_label():
+	health_label.text = "x%d" % current_health
+	
+# Update score
+func update_score_label():
+	score_label.text = "%06d" % current_score
+	
