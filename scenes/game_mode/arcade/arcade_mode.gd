d88f040e76488be5128400f3ae632332dfb2f85d
Truong-Son Nguyen <son.nt227148@sis.hust.edu.vn>
2024-12-27 08:07:11 +0700

    Add back button to Arcade select level

--- scenes/game_mode/arcade/arcade_mode.gd
+++ scenes/game_mode/arcade/arcade_mode.gd
@@ -1,9 +1,7 @@
 extends Control
 
-
-func _on_back_pressed():
-	SceneHistory.go_back()
-
-
 func _on_level_1_pressed() -> void:
 	SceneLoader.load_scene("res://scenes/game_mode/arcade/level/level.tscn")
+
+func _on_exit_pressed() -> void:
+	SceneHistory.go_back()
